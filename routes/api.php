5c3d8bd6f994f22e2f20b5b55ef6780ea718e5e2
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-04-22 16:42:53 +0200

    ajouter des controllers et des routes

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource("articles", App\Http\Controllers\API\ArticleController::class);
+
+Route::apiResource("categories", App\Http\Controllers\API\CategoryController::class);
+
+Route::apiResource("commands", App\Http\Controllers\API\CommandController::class);
+
+Route::apiResource("messages", App\Http\Controllers\API\MessageController::class);
+
+Route::apiResource("notifications", App\Http\Controllers\API\NotificationController::class);
+
+Route::apiResource("payments", App\Http\Controllers\API\PaymentController::class);
+
+Route::apiResource("pictures", App\Http\Controllers\API\PictureController::class);
+
+Route::apiResource("roles", App\Http\Controllers\API\RoleController::class);
+
+Route::apiResource("users", App\Http\Controllers\API\UserController::class);
