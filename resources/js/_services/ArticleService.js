577a39af5b43872fbc48c8cda5ebc18ca5786d1a
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-10-30 15:58:56 +0100

    modification de ArticleService + store article

--- resources/js/_services/ArticleService.js
+++ resources/js/_services/ArticleService.js
@@ -1,4 +1,9 @@
 import Axios from './CallerService';
 import { useArticleStore } from '../stores/Article';
 
-// récupérer les articles 
+// récupérer les articles
+export async function getArticle() {
+    const res = await Axios.get('/articles');
+
+    return res.data;
+}
--- resources/js/stores/Article.js
+++ resources/js/stores/Article.js
@@ -1,59 +1,22 @@
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import { getArticle } from '../_services/ArticleService';
 
 export const useArticleStore = defineStore('article', () => {
-    const article = ref({
-        id: -1,
-        user_id: -1,
-        category_id: -1,
-        title: '',
-        content: '',
-        price: '',
-    });
-
-    article.value.id = Number(localStorage.getItem('id'));
-    article.value.user_id = Number(localStorage.getItem('user_id'));
-    article.value.category_id = Number(localStorage.getItem('category_id'));
-    article.value.title = localStorage.getItem('title');
-    article.value.content = localStorage.getItem('content');
-    article.value.price = localStorage.getItem('price');
-
-    function setArticle(data) {
-        article.value.id = data.id;
-        article.value.user_id = data.user_id;
-        article.value.category_id = data.category_id;
-        article.value.title = data.title;
-        article.value.content = data.content;
-        article.value.price = data.price;
-        localStorage.setItem('id', String(data.id));
-        localStorage.setItem('user_id', String(data.user_id));
-        localStorage.setItem('category_id', String(data.category_id));
-        localStorage.setItem('title', String(data.title));
-        localStorage.setItem('content', String(data.content));
-        localStorage.setItem('price', String(data.price));
-    }
-
-    function clearArticle() {
-        setArticle({
-            id: -1,
-            user_id: -1,
-            category_id: -1,
-            title: '',
-            content: '',
-            price: '',
-        });
-
-        // localStorage.removeItem('id');
-        // localStorage.removeItem('user_id');
-        // localStorage.removeItem('category_id');
-        // localStorage.removeItem('title');
-        // localStorage.removeItem('content');
-        // localStorage.removeItem('price');
+    const newArticles = ref([]); 
+    
+    // Fonction pour récupérer les 10 derniers articles
+    async function latestArticles() {
+        try {
+            const data = await getArticle();
+            newArticles.value = data.articles.slice(-10).reverse();
+        } catch (error) {
+            console.error("Erreur lors de la récupération des articles :", error);
+        }
     }
 
     return {
-        article,
-        setArticle,
-        clearArticle
-    }
+        newArticles,
+        latestArticles
+    };
 });
